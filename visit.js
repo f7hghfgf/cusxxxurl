7e98c58d5de3e5bf699918d2b34c69ffe24a5334
f7hghfgf <sdasc4353@openlist.qzz.io>
2025-11-04 18:14:02 +0800

    Update visit.js

--- visit.js
+++ visit.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer'); // ✅ 使用完整版 Puppeteer
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
